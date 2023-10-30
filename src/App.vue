93d5aa72807ad6bf3bd30a3cae62ae7101b4e43b
aomsinn2000 <140079942+Bandidkw@users.noreply.github.com>
2023-10-30 17:58:31 +0700

    summary

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -9,20 +8,19 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
+  components:{
     HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style-type: none;
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,198 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="body-container">
+      
+<!---------------- top-web ---------------->
+
+      <div class="top-web">
+        <nav class="navbar">
+      <a href="">NBA BOOKING</a>
+      <button>Login</button>
+    </nav>
+      </div>
+
+<!---------------- content ---------------->
+
+    <div class="content">
+      <div class="info">
+        <h3>เป็นพาร์ทเนอร์กับเรา</h3>
+        <h2>พร้อมกับรับสิทธิพิเศษ</h2>
+        <div class="regis-btn">
+        <button>สมัครสมาชิก</button>
+      </div>
+
+<!---------------- hotel-logo ---------------->
+
+      <div class="hotel-logo">
+        <div class="hotel-box">
+          <img src="./hotel-logo/1.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/2.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/3.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/4.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/5.png" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/6.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/7.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/8.png" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/9.jpg" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/10.png" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/11.png" alt="">
+        </div>
+        <div class="hotel-box">
+          <img src="./hotel-logo/12.png" alt="">
+        </div>
+      </div>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+/*---------------- top-web ----------------*/
+.container{
+  width: 100%;
+  display: block;
+  align-items: center;
+  background-image: url("img/bg-img.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  overflow: hidden;
+  min-height: 100vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.top-web{
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+  padding: 0 5%;
+  backdrop-filter: blur(5px);
+  box-shadow: 0 1px 20px 1px lightblue;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.top-web a{
+  font-size: 1.5rem;
+  color: #fff;
 }
-a {
-  color: #42b983;
+.top-web button{
+  background-color: #b5179e;
+  color: #fff;
+  cursor: pointer;
+  width: 7.5rem;
+  font-size: 1.1rem;
+  height: 2.5rem;
+  border: none;
+  border-radius: 10px;
+  transition: transform .2s;
+}
+.top-web button:hover{
+  transform: scale(1.1);
+}
+.navbar{
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+/*---------------- content ----------------*/
+.content{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - 190px);
+}
+.body-container{
+  overflow: hidden;
+  min-height: 100vh;
+}
+.info{
+  width: 100%;
+  max-width: 700px;
+  min-height: calc(100vh - 350px);
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  border-radius: 20px;
+  background: rgba(0,0,0, 0.4);
+}
+.info h3{
+  color: #fff;
+  font-size: 2rem;
+  font-weight: normal;
+  margin-bottom: 1rem;
+}
+.info h2{
+  color: #fff;
+  font-size: 2.5rem;
+  margin-bottom: 5rem;
+}
+.regis-btn button{
+  background-color: rgba(0,0,0, 0.4);
+  color: #fff;
+  border: #fff 1px solid;
+  border-radius: 10px;
+  width: 9.5rem;
+  height: 2.8rem;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.2s;
+}
+/*---------------- hotel ----------------*/
+.hotel-logo{
+  margin-top: 5rem;
+  color: #fff;
+  display: flex;
+  padding: 0 5%;
+  width: 100%;
+  height: 70px;
+}
+.hotel-logo img{
+  background:#fff;
+  width: 100%;
+}
+.hotel-box{
+  padding: 3px;
+  width: 90px;
+}
+.hotel-box img{
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+}
+.regis-btn button:hover{
+  background-color: #b5179e;
+  transform: scale(1.1);
 }
 </style>
