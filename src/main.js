c54e91aaa1629d06fe0f0c76cbb1c71e61c4ab1f
nonin22222 <nonin31313@gmail.com>
2023-11-15 14:38:07 +0700

    15/11/23

--- src/main.js
+++ src/main.js
@@ -7,41 +7,41 @@ import router from './router';
 
 import store from "./store";
 import PrimeVue from 'primevue/config';
-// import "primeicons/primeicons.css";
-// import "primevue/resources/primevue.min.css"; //core css
-// import "primevue/resources/themes/lara-light-purple/theme.css"; //theme
+import "primeicons/primeicons.css";
+import "primevue/resources/primevue.min.css"; //core css
+import "primevue/resources/themes/lara-light-purple/theme.css"; //theme
 // //prime flex
-// import  "primeflex/primeflex.css";
+import  "primeflex/primeflex.css";
 const app = createApp(App); 
 
 ////
-// import Button from "primevue/button";
-// import Card from 'primevue/card';
-// import ConfirmationService from "primevue/confirmationservice";
-// import ConfirmDialog from "primevue/confirmdialog";
-// import Dialog from "primevue/dialog";
-// import Dropdown from 'primevue/dropdown';
-// import InputText from "primevue/inputtext";
-// import Menubar from 'primevue/menubar';
-// import Panel from 'primevue/panel';
-// import ProgressSpinner from "primevue/progressspinner";
-// import Steps from 'primevue/steps';
-// import Toast from "primevue/toast";
-// import ToastService from "primevue/toastservice";
+import Button from "primevue/button";
+import Card from 'primevue/card';
+import ConfirmationService from "primevue/confirmationservice";
+import ConfirmDialog from "primevue/confirmdialog";
+import Dialog from "primevue/dialog";
+import Dropdown from 'primevue/dropdown';
+import InputText from "primevue/inputtext";
+import Menubar from 'primevue/menubar';
+import Panel from 'primevue/panel';
+import ProgressSpinner from "primevue/progressspinner";
+import Steps from 'primevue/steps';
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
 
-// import Column from 'primevue/column';
-// import ColumnGroup from 'primevue/columngroup'; 
-// import DataTable from 'primevue/datatable';
-// import Row from 'primevue/row'; 
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup'; 
+import DataTable from 'primevue/datatable';
+import Row from 'primevue/row'; 
 
-// import InputNumber from 'primevue/inputnumber';
+import InputNumber from 'primevue/inputnumber';
 
-// import Textarea from 'primevue/textarea';
+import Textarea from 'primevue/textarea';
 
 
 app.use(PrimeVue);
-// app.use(ToastService);
-// app.use(ConfirmationService);
+app.use(ToastService);
+app.use(ConfirmationService);
 ////
 app.use(store);
 try {
@@ -57,22 +57,22 @@ try {
 
 
 ////
-// app.component('Button', Button);
-// app.component('InputText', InputText);
-// app.component('Card', Card);
-// app.component('Dropdown', Dropdown);
-// app.component('Steps', Steps);
-// app.component('Menubar', Menubar);
-// app.component('Panel', Panel);
-// app.component('Textarea', Textarea);
-// app.component('InputNumber', InputNumber);
-// app.component('Row', Row);
-// app.component('ColumnGroup', ColumnGroup);
-// app.component('Column', Column);
-// app.component('DataTable', DataTable);
-// app.component("Toast", Toast);
-// app.component("ConfirmDialog", ConfirmDialog);
-// app.component("ProgressSpinner", ProgressSpinner);
-// app.component("Dialog", Dialog);
+app.component('AppButton', Button);
+app.component('AppInputText', InputText);
+app.component('AppCard', Card);
+app.component('AppDropdown', Dropdown);
+app.component('AppSteps', Steps);
+app.component('AppMenubar', Menubar);
+app.component('AppPanel', Panel);
+app.component('AppTextarea', Textarea);
+app.component('AppInputNumber', InputNumber);
+app.component('AppRow', Row);
+app.component('AppColumnGroup', ColumnGroup);
+app.component('AppColumn', Column);
+app.component('AppDataTable', DataTable);
+app.component("AppToast", Toast);
+app.component("AppConfirmDialog", ConfirmDialog);
+app.component("AppProgressSpinner", ProgressSpinner);
+app.component("AppDialog", Dialog);
 app.mount('#app')
 
