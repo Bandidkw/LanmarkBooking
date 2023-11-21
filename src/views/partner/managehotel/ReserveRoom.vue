4c1bc15255d5cb1317f7dc4a01727a6f3c74a291
Best-Bandid <140079942+Bandidkw@users.noreply.github.com>
2023-11-21 09:24:08 +0700

    21/11

--- src/views/partner/managehotel/ReserveRoom.vue
+++ src/views/partner/managehotel/ReserveRoom.vue
@@ -25,21 +25,10 @@
         <Column field="phone_number" class="" header="เบอร์โทรติดต่อ" style="width: 10%;"> </Column>
         <Column field="price" class="" header="ราคา" style="width: 5%;"> </Column>
         <Column
-        field="statusbooking"
+        field=""
         class=""
         header="ปิด-เปิด"
-        style="width: 5%"
-        >
-
-          <template #body>
-            <label class="w-20 relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer">
-            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-400 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
-            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">เปิด</span>
-            </label>
-             
-          </template>
-          
+        style="width: 5%">
         </Column>
       </DataTable>
       </div>
