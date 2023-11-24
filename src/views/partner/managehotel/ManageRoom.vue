5dfa6fa2dfd9e6aa73979359e26679599e43a933
KimPanupong <110224293+KimPanupong@users.noreply.github.com>
2023-11-24 14:40:29 +0700

    editpartner

--- src/views/partner/managehotel/ManageRoom.vue
+++ src/views/partner/managehotel/ManageRoom.vue
@@ -1,213 +1,201 @@
 <template>
-  <div class="grid px-10 mt-3 ml-5 mr-5" >
+  <div class="grid px-10 mt-3 ml-5 mr-5">
     <div class="col-12 lg:col-12 border">
       <div class="text-center text-2xl">ข้อมูลห้อง</div>
       <div class="flex px-4 w-full item-center p-4" style="flex-direction: column; align-items: center;">
         <div class="flex justify-center">
-        <p class="w-40 text-center text-2xl text-bold m-0">สถานะ: {{ reservationEnabled ? 'เปิด' : 'ปิด' }}</p>
+          <p class="w-40 text-center text-2xl text-bold m-0">สถานะ: {{ reservationEnabled ? 'เปิด' : 'ปิด' }}</p>
+        </div>
+        <div class="card flex justify-center mt-2">
+          <InputSwitch v-model="reservationEnabled" />
+        </div>
       </div>
-      <div class="card flex justify-center mt-2">
-      <InputSwitch v-model="reservationEnabled"/>
-      </div>
-      </div>
-      <DataTable
-      :value="Array.isArray(item_product) ? item_product : []"
-      :paginator="true"
-      :rows="20"
-      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-      :rowsPerPageOptions="[5, 10, 25, 50, 75, 100]"
-      currentPageReportTemplate="แสดง {first} ถึง {last} จาก {totalRecords} สินค้าทั้งหมด"
-      responsiveLayout="stack">
-      <!-- ตรวจสอบว่ามีข้อมูลสินค้าหรือไม่ -->
-      <template #empty>
-        <p class="font-italic text-center text-5xl" style="color: #bd1616">
-          ไม่พบข้อมูลสินค้า
-        </p>
-      </template>
+      <DataTable :value="Array.isArray(item_product) ? item_product : []" :paginator="true" :rows="20"
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        :rowsPerPageOptions="[5, 10, 25, 50, 75, 100]"
+        currentPageReportTemplate="แสดง {first} ถึง {last} จาก {totalRecords} สินค้าทั้งหมด" responsiveLayout="stack">
+        <!-- ตรวจสอบว่ามีข้อมูลสินค้าหรือไม่ -->
+        <template #empty>
+          <p class="font-italic text-center text-5xl" style="color: #bd1616">
+            ไม่พบข้อมูลสินค้า
+          </p>
+        </template>
 
-      <Column field="name" header="ชื่อ" style="width: 10%;"></Column>
-      <Column field="image" header="Picture" style="width: 10%">
+        <Column field="name" header="ชื่อ" style="width: 10%;"></Column>
+        <Column field="image" header="Picture" style="width: 10%">
           <template #body="{ data }">
-            <img
-              v-if="Array.isArray(data.image) && data.image.length > 0"
-              :src="getImage(data.image)"
-              alt="ID Card"
-              width="200"
-              style="max-width: 100%; height: auto"
-            />
+            <img v-if="Array.isArray(data.image) && data.image.length > 0" :src="getImage(data.image)" alt="ID Card"
+              width="200" style="max-width: 100%; height: auto" />
             <div v-else>ไม่มีรูปภาพ</div>
           </template>
         </Column>
-      <Column field="statusbooking" header="คำอธิบาย" style="width: 10%;">    
-         <template #body="{ data }"> 
-             <div v-if="data.statusbooking===true" class=" bg-green-500 text-white text-center" style="width: 40%; border-radius: 1rem; padding: 0.5rem;">เปิดการจอง</div>
-             <div v-else class=" bg-red-500 text-white text-center" style=" width: 40%; border-radius: 1rem; padding: 0.5rem;">ปิดการจอง</div>
-         </template>
-         
-      </Column>
-      <Column field="address" class="" header="ที่อยู่" style="width: 10%;"> </Column>
-      <Column field="phone_number" class="" header="เบอร์โทรติดต่อ" style="width: 10%;"> </Column>
-      <Column field="price" class="" header="ราคา" style="width: 5%;">
-          
-      </Column>
-      <Column
-        :exportable="false"
-        header="เพิ่มเติม"
-        style="width: 15%"
-      >
+        <Column field="statusbooking" header="คำอธิบาย" style="width: 10%;">
+          <template #body="{ data }">
+            <div v-if="data.statusbooking === true" class=" bg-green-500 text-white text-center"
+              style="width: 40%; border-radius: 1rem; padding: 0.5rem;">เปิดการจอง</div>
+            <div v-else class=" bg-red-500 text-white text-center"
+              style=" width: 40%; border-radius: 1rem; padding: 0.5rem;">ปิดการจอง</div>
+          </template>
 
-        <template #body="item">
-        
-          <div v-if="item.data.status===true">
-               <updateadmin title="แก้ไขข้อมูล" :admin_id="item.data._id" :data="item.data"/>
-              <Button
-                @click="deleteProduct(item.data._id)"
+        </Column>
+        <Column field="address" class="" header="ที่อยู่" style="width: 10%;"> </Column>
+        <Column field="phone_number" class="" header="เบอร์โทรติดต่อ" style="width: 10%;"> </Column>
+        <Column field="price" class="" header="ราคา" style="width: 5%;">
+
+        </Column>
+        <Column :exportable="false" header="เพิ่มเติม" style="width: 15%">
+
+          <template #body="item">
+
+            <div v-if="item.data.status === true">
+              <EditHotel title="แก้ไขข้อมูล" :partner_id="item.data._id" :data="item.data" />
+              <Button @click="deleteProduct(item.data._id)"
                 class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                style="background-color: #C21010"
-              >ลบ</Button>
-          </div>
-          <div v-else>
-             กรุณารออนุมัติจาก admin 
-          </div>
-         
-           
-        </template>
-      </Column>
-    </DataTable>
+                style="background-color: #C21010">ลบ</Button>
+            </div>
+            <div v-else>
+              กรุณารออนุมัติจาก admin
+            </div>
+
+
+          </template>
+        </Column>
+      </DataTable>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { onMounted,watch, ref } from "vue";
+import { onMounted, watch, ref } from "vue";
 import Swal from "sweetalert2";
-import updateadmin from '@/views/admin/manageadmin/EditAdmin.vue';
+import EditHotel from "./EditHotel.vue";
 
 export default {
-components: {
-  updateadmin,
-},
+  components: {
+    EditHotel,
+  },
 
 
-created() {
-  document.title = "จัดการข้อมูลห้อง";
-},
+  created() {
+    document.title = "จัดการข้อมูลห้อง";
+  },
 
 
-setup() {
-  const item_product = ref([]);
-  const reservationEnabled = ref(false);
+  setup() {
+    const item_product = ref([]);
+    const reservationEnabled = ref(false);
 
-  const getData = async () => {
-    try {
-      const productResponse = await axios.get(
-        `${process.env.VUE_APP_API}room/partner/`,
-        {
-          headers: {
-            token: localStorage.getItem("token"),
-          },
-        }
-      );
-      if (productResponse.data && productResponse.data) {
+    const getData = async () => {
+      try {
+        const productResponse = await axios.get(
+          `${process.env.VUE_APP_API}room/partner/`,
+          {
+            headers: {
+              token: localStorage.getItem("token"),
+            },
+          }
+        );
+        if (productResponse.data && productResponse.data) {
           item_product.value = productResponse.data;
-          console.log(productResponse.data)  
-          reservationEnabled.value=productResponse.data[0].statusbooking
+          console.log(productResponse.data)
+          reservationEnabled.value = productResponse.data[0].statusbooking
 
-      } else {
-        console.error("Data is missing in the API response.");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-
-  const deleteProduct = async (_id) => {
-    try {
-      const response = await axios.delete(
-        `${process.env.VUE_APP_API}room/${_id}`,
-        {
-          headers: {
-            token: localStorage.getItem("token"),
-          },
+        } else {
+          console.error("Data is missing in the API response.");
         }
-      );
-      if (response.data) {
-        // หากการลบสำเร็จ อัปเดตข้อมูล
-        getData()
-        // แสดงข้อความสำเร็จ (ตัวเลือก)
-        Swal.fire({
-          icon: "success",
-          title: "ลบสินค้าสำเร็จ",
-        });
-      } else {
-        await Swal.fire({
-          icon: "error",
-          title: "เกิดข้อผิดพลาด",
-          text: "ไม่สามารถแก้ไขข้อมูลได้",
-        });
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      await Swal.fire({
+    };
+
+
+    const deleteProduct = async (_id) => {
+      try {
+        const response = await axios.delete(
+          `${process.env.VUE_APP_API}room/${_id}`,
+          {
+            headers: {
+              token: localStorage.getItem("token"),
+            },
+          }
+        );
+        if (response.data) {
+          // หากการลบสำเร็จ อัปเดตข้อมูล
+          getData()
+          // แสดงข้อความสำเร็จ (ตัวเลือก)
+          Swal.fire({
+            icon: "success",
+            title: "ลบสินค้าสำเร็จ",
+          });
+        } else {
+          await Swal.fire({
+            icon: "error",
+            title: "เกิดข้อผิดพลาด",
+            text: "ไม่สามารถแก้ไขข้อมูลได้",
+          });
+        }
+      } catch (error) {
+        await Swal.fire({
           icon: "error",
           title: "เกิดข้อผิดพลาด",
           text: "ไม่สามารถลบข้อมูลได้",
         });
-    }
-  };
+      }
+    };
 
 
-  onMounted(() => {
-    getData();
-  });
+    onMounted(() => {
+      getData();
+    });
 
 
-  watch(
-     ()=> reservationEnabled.value,
-    async (newValue) => {
-      try{
-        if(newValue==true)
-        {
-          await axios.put(`${process.env.VUE_APP_API}room/openstatus/`,{},
-          {
-            headers: {
-              token: localStorage.getItem("token"),
-            },})
-         
+    watch(
+      () => reservationEnabled.value,
+      async (newValue) => {
+        try {
+          if (newValue == true) {
+            await axios.put(`${process.env.VUE_APP_API}room/openstatus/`, {},
+              {
+                headers: {
+                  token: localStorage.getItem("token"),
+                },
+              })
+
             getData()
-        }else{
-          await axios.put(`${process.env.VUE_APP_API}room/closestatus/`,{},
-          {
-            headers: {
-              token: localStorage.getItem("token"),
-            },})
-            
+          } else {
+            await axios.put(`${process.env.VUE_APP_API}room/closestatus/`, {},
+              {
+                headers: {
+                  token: localStorage.getItem("token"),
+                },
+              })
+
             getData()
+          }
         }
-      }
-      catch(error){
+        catch (error) {
+
+        }
 
       }
-     
-    }
-  );
+    );
 
 
 
 
-  return {
-    reservationEnabled,
-    item_product,
-    getData,
-    deleteProduct,
-  };
-},
+    return {
+      reservationEnabled,
+      item_product,
+      getData,
+      deleteProduct,
+    };
+  },
 
 
-methods: {
+  methods: {
 
-  getImage(item){
+    getImage(item) {
       if (typeof item === 'string') {
         return `https://drive.google.com/uc?export=view&id=${item}`;
       } else if (Array.isArray(item) && item.length > 0) {
@@ -216,25 +204,25 @@ methods: {
       } else {
         return "";
       }
-},
+    },
 
-//------------------------ เปิด-ปิดการจองห้อง------------------//
-// toggleReservationStatus() {
-//   console.log("hello")
-// this.reservationEnabled = !this.reservationEnabled;
-// if (this.reservationEnabled) {
-//   console.log("Open");
-//   fetchData();
+    //------------------------ เปิด-ปิดการจองห้อง------------------//
+    // toggleReservationStatus() {
+    //   console.log("hello")
+    // this.reservationEnabled = !this.reservationEnabled;
+    // if (this.reservationEnabled) {
+    //   console.log("Open");
+    //   fetchData();
 
-// } else {
-//   console.log("Off");
-//   collectData();
-// }
-// },
-},
+    // } else {
+    //   console.log("Off");
+    //   collectData();
+    // }
+    // },
+  },
 
 
-name: 'ManageRoom',
+  name: 'ManageRoom',
 };
 
 </script>
--- src/views/partner/managepartner/EditPartner.vue
+++ src/views/partner/managepartner/EditPartner.vue
@@ -1,114 +1,204 @@
 <template>
-  <Button @click="getdata" class="hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2" style="background-color: #ff7315" :label="title" :loading="loading" />
+  <!-- <Button @click="getdata" class="hover:bg-red-700 text-white font-bold py-2 px-4 rounded mx-2"
+    style="background-color: #ff7315" :label="title" :loading="loading" /> -->
 
   <!--eslint-disable-next-line vue/no-multiple-template-root -->
-  <Dialog v-model:visible="sidebar" modal :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }" >
-    <div class="grid">
-      <div class="col-12 text-center">
-        <h2>แก้ไขข้อมูล admin</h2>
+  <!-- <Dialog v-model:visible="sidebar" modal :style="{ width: '50rem' }"
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"> -->
+  <div class="grid">
+    <div class="col-12 text-center">
+      <h2>Edit Partner Information</h2>
+      <div class="col-12 flex justify-content-center">
+        <img v-if="image_card" :src="getImage(image_card)" alt="ID Card" style="  max-width: 50%; height: 50%" />
+        <div v-else>ไม่มีรูปภาพ</div>
       </div>
     </div>
-    <div class="grid">
-      <div class="col-12 md:col-12">
-          <form class="">
-            <div class="col-12">
-                <p>เบอร์โทรศัพท์ :  </p>
-                <InputText v-model="telephone" name="telephone" placeholder="000-0000-00000" class="w-full " />
-            </div>
-            <div class="col-12">
-                <p>password :(ถ้าไม่ได้ต้องแก้ไม่ต้องใส่)</p>
-                <InputText type="password"  class="w-full"  v-model="password" name="password" placeholder="*****" />
-            </div>
-            <div class="col-12">
-                <p> ชื่อ :</p>
-                <InputText v-model="name" name="name" placeholder="กรุณากรอกชื่อ" class="w-full" />
-            </div>
-          </form>
-      </div>
-    </div>
-    <div class="grid">
-            <div class="col-12 text-center mt-2">
-                <Button label="แก้ไข" @click="editadmin" />
-            </div>
+  </div>
+  <div class="grid">
+    <div class="col-12 md:col-12">
+      <form class="">
+        <div class="col-12">
+          <p>เบอร์โทรศัพท์ : </p>
+          <InputText v-model="telephone" name="telephone" placeholder="000-0000-00000" class="w-full " />
         </div>
-  </Dialog>
+        <div class="col-12">
+          <p> ID Card :</p>
+          <InputText v-model="idcard" name="idcard" class="w-full" />
+        </div>
+
+        <div class="col-12">
+          <p> ชื่อ :</p>
+          <InputText v-model="name" name="name" placeholder="กรุณากรอกชื่อ" class="w-full" />
+        </div>
+        <div class="col-12">
+          <p> ที่อยู่ :</p>
+          <InputText v-model="address" name="address" class="w-full" />
+        </div>
+        <div class="col-12">
+          <p> Province :</p>
+          <Dropdown
+            class="appearance-none  w-full text-gray-700 border border-bluegray-800 rounded py-1 px-2 mb-3 leading-tight focus:outline-none focus:bg-white"
+            v-model="province" :options="provincedropdown.value" optionLabel="name_th" optionValue="name_th"
+            placeholder="เลือกจังหวัด" @change="getamphure('amphure')" />
+        </div>
+        <div class="col-12">
+          <p> Amphure :</p>
+          <Dropdown
+            class="appearance-none w-full text-gray-700 border border-bluegray-800 rounded py-1 px-2 mb-3 leading-tight focus:outline-none focus:bg-white"
+            v-model="amphure" :disabled="!province" :options="amphuredropdown.value" optionLabel="name_th"
+            optionValue="name_th" placeholder="เลือกอำเภอ" @change="getamphure('tambon')" />
+        </div>
+        <div class="col-12">
+          <p> Tambon :</p>
+          <Dropdown
+            class="appearance-none w-full text-gray-700 border border-bluegray-800 rounded py-1 px-2 mb-3 leading-tight focus:outline-none focus:bg-white"
+            v-model="tambon" :disabled="!amphure" :options="tambondropdown.value" optionLabel="name_th"
+            optionValue="name_th" placeholder="เลือกตำบล" />
+        </div>
+        <div class="col-12">
+          <p>password :</p>
+          <InputText type="password" class="w-full" v-model="password" name="password" placeholder="*****" />
+        </div>
+      </form>
+    </div>
+  </div>
+  <div class="grid">
+    <div class="col-12 text-center mt-2">
+      <Button label="แก้ไข" @click="editadmin" />
+    </div>
+  </div>
+  <!-- </Dialog> -->
 </template>
 
 <script>
 import Swal from "sweetalert2";
 import axios from 'axios';
+import { ref, onMounted } from "vue";
+
 export default {
   props: {
-    shop_id: String,
     data: Object,
     title: String,
   },
-  data: () => ({
-    loading: false,
-    sidebar: false,
-    telephone:'',
-    password:'',
-    name:'',
-  }),
+  data() {
+
+    const provincedropdown = ref([]);
+    const amphuredropdown = ref([null]);
+    const tambondropdown = ref([null]);
+    const getprovince = async () => {
+      try {
+        const province = await axios.get(
+          `${process.env.VUE_APP_THAILAND}thailand/province`
+        );
+        this.provincedropdown.value = province.data;
+        console.log(this.provincedropdown.value, 'province');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    onMounted(() => {
+      getprovince();
+    });
+    return {
+      provincedropdown,
+      amphuredropdown,
+      tambondropdown,
+      // loading: false,
+      // sidebar: false,
+      telephone: '',
+      password: '',
+      name: '',
+      address: '',
+      province: '',
+      tambon: '',
+      amphure: '',
+      idcard: '',
+      image_card: '',
+    }
+  },
   methods: {
     async getdata() {
-      const id = this.data._id;
       this.sidebar = true;
-      try{
-        const res = await axios.get(`${process.env.VUE_APP_API}admin/${id}`,
-          {headers: {token: localStorage.getItem("token"),},}
-        );
-        this.telephone =res?.data?.data?.telephone
-        this.name = res?.data?.data?.name
-        
-      }catch(error){
-        console.log(error)
-      }
 
+      try {
+        const res = await axios.post(`${process.env.VUE_APP_API}partner/findpartner`, {
+          headers: {
+            token: localStorage.getItem("token"),
+          },
+        });
+
+        console.log(res.data.data, "res data");
+
+        if (res.data && res.data.data) {
+          const partnerData = res.data.data;
+
+          this.telephone = partnerData.telephone;
+          this.name = partnerData.name;
+          this.idcard = partnerData.idcard;
+          this.address = partnerData.address;
+          this.image_card = partnerData.image_card;
+          this.password = partnerData.password;
+          this.province = partnerData.province;
+
+          const getamphure = await this.getamphure("amphure");
+          if (getamphure === true) {
+            console.log(this.amphuredropdown);
+            this.amphure = partnerData.amphure;
+          }
+
+          const gettambon = await this.getamphure("tambon");
+          if (gettambon === true) {
+            this.tambon = partnerData.tambon;
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
     },
-    async editadmin(){
-      if (this.telephone === null || this.telephone === "" ||this.name ==="") {
+    async editadmin() {
+      if (this.name === "" || this.name === null) {
         this.sidebar = false;
         await Swal.fire({
-            icon: "error",
-            title: "กรอกข้อมูลไม่ครบ",
-            text: "กรุณากรอกข้อมูลให้ครบ",
-          });
-          this.sidebar = true;
-      }else{
+          icon: "error",
+          title: "กรอกข้อมูลไม่ครบ",
+          text: "กรุณากรอกข้อมูลให้ครบ",
+        });
+        this.sidebar = true;
+      } else {
         try {
           const id = this.data._id;
-          const res = await axios.put(`${process.env.VUE_APP_API}admin/${id}`, {
+          const res = await axios.put(`${process.env.VUE_APP_API}partner/${id}`, {
             telephone: this.telephone,
             password: this.password,
-            name:this.name,
-            roles:"admin",
-            level:"1"
-          },{
+            name: this.name,
+            roles: "admin",
+            level: "1"
+          }, {
             headers: {
               token: localStorage.getItem("token"),
             },
           });
           if (res.data) {
             this.sidebar = false;
-          await Swal.fire({
-            icon: "success",
-            title: "แก้ไขข้อมูลสำเร็จ",
-            text: "ข้อมูลแก้ไขข้อมูลเรียบร้อย",
-          });
-          
+            await Swal.fire({
+              icon: "success",
+              title: "แก้ไขข้อมูลสำเร็จ",
+              text: "ข้อมูลแก้ไขข้อมูลเรียบร้อย",
+            });
+
           } else {
             this.sidebar = false;
             await Swal.fire({
-            icon: "error",
-            title: "เกิดข้อผิดพลาด",
-            text: "ไม่สามารถแก้ไขข้อมูลได้",
-          });
-          this.sidebar = true;
+              icon: "error",
+              title: "เกิดข้อผิดพลาด",
+              text: "ไม่สามารถแก้ไขข้อมูลได้",
+            });
+            this.sidebar = true;
           }
-        }catch(error){
+        } catch (error) {
           this.sidebar = false;
-            await Swal.fire({
+          await Swal.fire({
             icon: "error",
             title: "เกิดข้อผิดพลาด",
             text: "ไม่สามารถแก้ไขข้อมูลได้",
@@ -116,7 +206,54 @@ export default {
           this.sidebar = true;
         }
       }
+    },
+    async getamphure(type) {
+      try {
+        if (type === "amphure") {
+          const selectedProvinceObject = this.provincedropdown.value.find(
+            (province) => province.name_th === this.province
+          );
+          const id = selectedProvinceObject.id;
+
+          const amphure = await axios.get(
+            `${process.env.VUE_APP_THAILAND}thailand/amphure/by-province-id/${id}`
+          );
+          this.amphuredropdown.value = amphure.data;
+          return true;
+        }
+
+        if (type === "tambon") {
+          const selectedAmphureObject = this.amphuredropdown.value.find(
+            (amphure) => amphure.name_th === this.amphure
+          );
+
+          const id = selectedAmphureObject.id;
+
+          const tambon = await axios.get(
+            `${process.env.VUE_APP_THAILAND}thailand/tambon/by-amphure-id/${id}`
+          );
+          this.tambondropdown.value = tambon.data;
+          return true;
+        }
+      } catch (error) {
+        console.error(error);
+        return false; // Indicate that there was an error
+      }
+    },
+
+    getImage(item) {
+      if (typeof item === 'string') {
+        return `https://drive.google.com/uc?export=view&id=${item}`;
+      } else if (Array.isArray(item) && item.length > 0) {
+        const firstImageId = item[0];
+        return `https://drive.google.com/uc?export=view&id=${firstImageId}`;
+      } else {
+        return "";
+      }
     }
+  },
+  created() {
+    this.getdata()
   }
 };
 
