185791f404e7eedab4d0276fe74716c5abccf57e
natthanon_sornsuwan <nonin31313@gmail.com>
2023-12-02 15:56:31 +0700

    2/12/23

--- src/views/admin/manageadmin/ManageAdmin.vue
+++ src/views/admin/manageadmin/ManageAdmin.vue
@@ -1,42 +1,70 @@
-
 <template>
   <div class="grid px-10 mt-3 ml-5 mr-5 w-full containter">
     <div class="col-12 lg:col-12 border">
       <div class="text-center font-bold text-4xl">จัดการข้อมูล admin</div>
       <div class="text-right my-5">
         <router-link to="/addadmin">
-
-          <Button label="เพิ่มข้อมูลadmin"
-            class="bg-blue-500 border-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" />
+          <Button
+            label="เพิ่มข้อมูลadmin"
+            class="bg-blue-500 border-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          />
         </router-link>
-
       </div>
 
-      <DataTable :value="Array.isArray(item_product) ? item_product : []" :paginator="true" :rows="20"
+      <DataTable
+        :value="Filter"
+        :paginator="true"
+        :rows="20"
         paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-        :rowsPerPageOptions="[5, 10, 25, 50, 75, 100]" :loading="loading"
-        currentPageReportTemplate="แสดง {first} ถึง {last} จาก {totalRecords} สินค้าทั้งหมด" responsiveLayout="stack">
+        :rowsPerPageOptions="[5, 10, 25, 50, 75, 100]"
+        :loading="loading"
+        currentPageReportTemplate="แสดง {first} ถึง {last} จาก {totalRecords} สินค้าทั้งหมด"
+        responsiveLayout="stack"
+      >
         <!-- ตรวจสอบว่ามีข้อมูลสินค้าหรือไม่ -->
         <template #empty>
           <p class="font-italic text-center text-5xl" style="color: #bd1616">
             ไม่พบข้อมูลสินค้า
           </p>
         </template>
+        <template #header>
+          <div class="flex justify-content-end">
+            <span class="p-input-icon-left">
+              <i class="pi pi-search" />
+              <InputText
+                v-model="searchall"
+                placeholder="ค้นหา"
+                class="bg-white-500 p-2 pl-5 border"
+              />
+            </span>
+          </div>
+        </template>
 
-        <Column field="telephone" header="เบอร์โทรศัพท์" style="width: 20%;"></Column>
-        <Column field="name" class="" header="ชื่อ" style="width: 10%;"> </Column>
-        <Column :exportable="false" class="" header="เพิ่มเติม" style="width: 10%">
-
+        <Column
+          field="telephone"
+          header="เบอร์โทรศัพท์"
+          sortable
+          style="width: 20%"
+        ></Column>
+        <Column field="name" class="" header="ชื่อ" sortable style="width: 10%">
+        </Column>
+        <Column
+          :exportable="false"
+          class=""
+          header="เพิ่มเติม"
+          style="width: 10%"
+        >
           <template #body="item">
             <updateadmin title="แก้ไขข้อมูล" :data="item.data" />
-            <Button @click="deleteProduct(item.data._id)"
+            <Button
+              @click="deleteProduct(item.data._id)"
               class="bg-red-500 hover:bg-red-700 text-white font-bold border-none py-2 px-4 rounded"
-              style="background-color: #C21010">ลบ</Button>
-
+              style="background-color: #c21010"
+              >ลบ</Button
+            >
           </template>
         </Column>
       </DataTable>
-
     </div>
   </div>
 </template>
@@ -45,19 +73,19 @@
 import axios from "axios";
 import { onMounted, ref } from "vue";
 import Swal from "sweetalert2";
-import updateadmin from '@/views/admin/manageadmin/EditAdmin.vue'
+import updateadmin from "@/views/admin/manageadmin/EditAdmin.vue";
 
 export default {
   components: {
-    updateadmin
+    updateadmin,
   },
   created() {
     document.title = "จัดการข้อมูล admin";
   },
   setup() {
     const item_product = ref([]);
-
-    let loading = ref(true)
+    let loading = ref(true);
+    const searchall = ref('');
     const getData = async () => {
       try {
         const productResponse = await axios.get(
@@ -70,9 +98,9 @@ export default {
         );
 
         if (productResponse.data && productResponse.data) {
-          loading.value = false
+          loading.value = false;
           item_product.value = productResponse.data.data;
-          console.log(productResponse.data.data)
+          console.log(productResponse.data.data);
         } else {
           console.error("Data is missing in the API response.");
         }
@@ -93,7 +121,7 @@ export default {
 
         if (response.data) {
           // หากการลบสำเร็จ อัปเดตข้อมูล
-          getData()
+          getData();
           // แสดงข้อความสำเร็จ (ตัวเลือก)
           Swal.fire({
             icon: "success",
@@ -114,6 +142,7 @@ export default {
         });
       }
     };
+
     onMounted(() => {
       getData();
     });
@@ -121,11 +150,28 @@ export default {
       item_product,
       getData,
       deleteProduct,
-      loading
+      loading,
+      searchall,
     };
-
   },
-  name: 'ManageAdmin',
+  computed: {
+  Filter() {
+    if (this.searchall) {
+        const searchTerm = this.searchall.toLowerCase();
+        return this.item_product.filter(item => {
+          // ใช้ includes() เพื่อตรวจสอบว่าคำที่ค้นหาอยู่ในชื่อหรือเบอร์โทรศัพท์หรือไม่
+          return (
+            item.name.toLowerCase().includes(searchTerm) ||
+            item.telephone.includes(searchTerm)
+          );
+        });
+      } else {
+        return this.item_product;
+      }
+  },
+},
+
+  name: "ManageAdmin",
 };
 </script>
 <style scoped>
@@ -133,4 +179,3 @@ export default {
 @import "tailwindcss/components";
 @import "tailwindcss/utilities";
 </style>
-
