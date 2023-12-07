56531b1be66dcfde776085879521e6a2093b8a46
Best-Bandid <140079942+Bandidkw@users.noreply.github.com>
2023-12-07 16:51:15 +0700

    7/12

--- src/components/Contract_Modal.vue
+++ src/components/Contract_Modal.vue
@@ -173,8 +173,6 @@
   </div>
 </template>
 <script>
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
 import axios from "axios";
 export default {
   data() {
@@ -200,27 +198,6 @@ export default {
     this.getcontract();
   },
   methods: {
-    download() {
-      const doc = new jsPDF();
-      const contentHtml = this.$refs.content.innerHTML;
-      doc.fromHTML(contentHtml, 15, 15, {
-        width: 170,
-      });
-      doc.save("sample.pdf");
-    },
-
-    downloadWithCSS() {
-      const doc = new jsPDF();
-      /** WITH CSS */
-      var canvasElement = document.createElement("canvas");
-      html2canvas(this.$refs.content, { canvas: canvasElement }).then(function (
-        canvas
-      ) {
-        const img = canvas.toDataURL("image/jpeg", 0.8);
-        doc.addImage(img, "JPEG", 20, 20);
-        doc.save("sample.pdf");
-      });
-    },
     async getcontract() {
       try {
         const Response = await axios.get(
